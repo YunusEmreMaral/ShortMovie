aa4022eb66a39839de67f27a53eae39cb889c8e7
Yunus Emre Maral <maralyunus41@gmail.com>
2024-05-01 00:08:49 +0300

    Business Katmanı Temel Bitirilmesi

---
+++ BusinessLayer/Concrete/DirectorManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class DirectorManager:IDirectorService
+    {
+        IDirectorDal _directorDal;
+
+        public DirectorManager(IDirectorDal directorDal)
+        {
+            _directorDal = directorDal;
+        }
+
+        public void TAdd(Director t)
+        {
+            _directorDal.Insert(t);
+        }
+
+        public void TDelete(Director t)
+        {
+            _directorDal.Delete(t);
+        }
+
+        public Director TGetByID(int id)
+        {
+           return _directorDal.GetByID(id);
+        }
+
+        public List<Director> TGetList()
+        {
+            return _directorDal.GetList();
+        }
+
+        public void TUpdate(Director t)
+        {
+            _directorDal.Update(t);
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/MovieManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class MovieManager : IMovieService
+    {
+        IMovieDal _movieDal;
+
+        public MovieManager(IMovieDal movieDal)
+        {
+            _movieDal = movieDal;
+        }
+
+        public void TAdd(Movie t)
+        {
+            _movieDal.Insert(t);
+        }
+
+        public void TDelete(Movie t)
+        {
+            _movieDal.Delete(t);
+        }
+
+        public Movie TGetByID(int id)
+        {
+            return _movieDal.GetByID(id);
+        }
+
+        public List<Movie> TGetList()
+        {
+            return _movieDal.GetList();
+        }
+
+        public void TUpdate(Movie t)
+        {
+            _movieDal.Update(t);
+        }
+    }
+}
